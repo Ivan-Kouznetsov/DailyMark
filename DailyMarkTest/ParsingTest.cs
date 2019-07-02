52ca3a9ef9e716cc56e39dec99dc84fbda96353b
Ivan <ivankuz@hotmail.com>
2019-07-02 12:53:07 -0700

    using zipped xml file for testing

--- DailyMarkTest/ParsingTest.cs
+++ DailyMarkTest/ParsingTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DailyMark;
 using System.IO;
+using System.IO.Compression;
 using System;
 
 namespace DailyMarkTest
@@ -10,11 +11,15 @@ namespace DailyMarkTest
     {
         [TestMethod]
         public void TestXMLParsing()
-        { 
-            Stream stream =  File.Open("apc190401.xml",FileMode.Open);
-            var results = DailyMark.Services.BdssParser.ParseXML(stream, DailyMark.DAO.LocalDAO.StatusCodes, DateTime.MinValue);
-            Assert.AreEqual(results.deadApps.Count, 5892);
-            Assert.AreEqual(results.newApps.Count, 4354);
+        {
+            using (Stream zipFileStream = File.Open("apc190401.zip", FileMode.Open))
+            {
+                ZipArchive zipArchive = new ZipArchive(zipFileStream, ZipArchiveMode.Read);
+
+                var results = DailyMark.Services.BdssParser.ParseXML(zipArchive.Entries[0].Open(), DailyMark.DAO.LocalDAO.StatusCodes, DateTime.MinValue);
+                Assert.AreEqual(results.deadApps.Count, 5892);
+                Assert.AreEqual(results.newApps.Count, 4354);
+            }
         }
     }
 }
